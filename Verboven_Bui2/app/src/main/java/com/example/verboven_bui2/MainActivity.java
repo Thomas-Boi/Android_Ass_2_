1897f22459b97aa78962a2c6138d5348598dedb8
Thomas Bui <thomasbui198@gmail.com>
2019-11-04 19:15:48 -0800

    Added a simple layout for adding reading

--- Verboven_Bui2/app/src/main/java/com/example/verboven_bui2/MainActivity.java
+++ Verboven_Bui2/app/src/main/java/com/example/verboven_bui2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.verboven_bui2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onAddReading(View v) {
+        openAddPopUp();
+    }
+
+    protected void openAddPopUp() {
+
+    }
+
+    public void onCancel(View v) {
+    }
+
+    public void onSubmit(View v) {
+        addReading();
+    }
+
+    protected void addReading() {
+
     }
 }
