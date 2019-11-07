66b4d3e90ce87bd022c9d0c17ae4e522ad8dffe8
Thomas Bui <thomasbui198@gmail.com>
2019-11-06 19:48:27 -0800

    Added Firebase Realtime DB dependency

--- Verboven_Bui2/app/src/main/java/com/example/verboven_bui2/MainActivity.java
+++ Verboven_Bui2/app/src/main/java/com/example/verboven_bui2/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.verboven_bui2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void openAddPopUp() {
+        System.out.println("Running");
+        AlertDialog.Builder diaglogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
 
+        View addReadingView = inflater.inflate(R.layout.activity_add_reading_dialog, null);
+        diaglogBuilder.setView(addReadingView);
+
+        AlertDialog addReadingDialog = diaglogBuilder.create();
+        addReadingDialog.show();
     }
 
     public void onCancel(View v) {
---
+++ Verboven_Bui2/app/src/main/java/com/example/verboven_bui2/Reading.java
@@ -0,0 +1,4 @@
+package com.example.verboven_bui2;
+
+public class Reading {
+}
